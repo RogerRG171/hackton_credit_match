c2739453900f64ef10b90387206b72a68388be31
Roger G Nascimento <roger.goulart.nascimento@gmail.com>
2022-07-29 11:47:52 -0300

    add Environment Variable for PORT

--- src/database/dbConfig.js
+++ src/database/dbConfig.js
@@ -12,7 +12,7 @@ const connectToPostgres = async () =>{
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        port: 5432 
+        port: process.env.DB_PORT 
     }
 
    
